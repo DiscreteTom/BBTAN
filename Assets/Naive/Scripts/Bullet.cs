ae1689fcb2d6e300d0852167feecad1477022ccd
DiscreteTom <discrete_tom@outlook.com>
2022-09-13 21:19:39 +0800

    update score/highScore text

--- Assets/Naive/Scripts/Bullet.cs
+++ Assets/Naive/Scripts/Bullet.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace BBTAN.Naive {
   public class Bullet : MonoBehaviour {
+    TMP_Text scoreText;
+    TMP_Text highScoreText;
+
+    void Start() {
+      this.scoreText = GameObject.Find("ScoreText").GetComponent<TMP_Text>();
+      this.highScoreText = GameObject.Find("HighScoreText").GetComponent<TMP_Text>();
+    }
+
     public void SetVelocity(Vector2 v) {
       this.GetComponent<Rigidbody2D>().velocity = v;
     }
@@ -17,6 +26,12 @@ namespace BBTAN.Naive {
         if (GameManager.ballDestroyed == GameManager.ballCount) {
           GameManager.shooting = false;
           GameManager.ballCount = GameManager.nextBallCount;
+          GameManager.score++;
+          this.scoreText.text = GameManager.score.ToString();
+          if (GameManager.score > GameManager.highScore) {
+            GameManager.highScore = GameManager.score;
+            this.highScoreText.text = "TOP:" + GameManager.highScore.ToString();
+          }
         }
       }
     }
