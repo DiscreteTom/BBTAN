447ffa3d36c139e9ec68ece3424bc99b3d0c484b
DiscreteTom <discrete_tom@outlook.com>
2022-09-13 21:34:03 +0800

    finish block/diamond

---
+++ Assets/Naive/Scripts/Block.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace BBTAN.Naive {
+  public class Block : MonoBehaviour {
+    int health = 1;
+    TMP_Text text;
+
+    void Start() {
+      this.text = this.transform.Find("Canvas/HealthText").GetComponent<TMP_Text>();
+    }
+
+    public void SetHealth(int n) {
+      this.health = n;
+      this.text.text = n.ToString();
+    }
+
+    void OnCollisionEnter2D(Collision2D c) {
+      this.health--;
+      this.text.text = this.health.ToString();
+
+      if (this.health == 0) Destroy(this.gameObject);
+    }
+  }
+}
