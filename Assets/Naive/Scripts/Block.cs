1a3a223bf580cc6c9642358fa9226f93f990ba0c
DiscreteTom <discrete_tom@outlook.com>
2022-09-13 22:01:11 +0800

    finish random generate props

--- Assets/Naive/Scripts/Block.cs
+++ Assets/Naive/Scripts/Block.cs
@@ -13,6 +13,7 @@ namespace BBTAN.Naive {
     }
 
     public void SetHealth(int n) {
+      this.text = this.transform.Find("Canvas/HealthText").GetComponent<TMP_Text>();
       this.health = n;
       this.text.text = n.ToString();
     }
