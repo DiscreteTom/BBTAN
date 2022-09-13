df1079a111e3b37871e0c9d2da2a1ed974522be4
DiscreteTom <discrete_tom@outlook.com>
2022-09-13 22:31:47 +0800

    generate level 1

--- Assets/Naive/Scripts/Bullet.cs
+++ Assets/Naive/Scripts/Bullet.cs
@@ -5,13 +5,6 @@ using UnityEngine;
 
 namespace BBTAN.Naive {
   public class Bullet : MonoBehaviour {
-    [SerializeField] int blockCount = 5;
-    [SerializeField] GameObject block;
-    [SerializeField] GameObject diamondBlock;
-    [SerializeField] GameObject addBulletProp;
-    [SerializeField] float initBlockY = 5;
-    [SerializeField] float blockSpacing = 0.5f;
-
     TMP_Text scoreText;
     TMP_Text highScoreText;
 
@@ -47,40 +40,7 @@ namespace BBTAN.Naive {
             this.highScoreText.text = "TOP:" + GameManager.highScore.ToString();
           }
           GameObject.Find("Shooter").GetComponent<Shooter>().Show();
-          this.UpdateProps();
-        }
-      }
-    }
-
-    void UpdateProps() {
-      var props = GameObject.Find("Props");
-      // move all existing props
-      for (var i = 0; i < props.transform.childCount; ++i) {
-        props.transform.GetChild(i).transform.Translate(Vector2.down * this.blockSpacing);
-      }
-
-      // index of add bullet prop
-      var abp = Random.Range(0, this.blockCount);
-
-      for (var i = 0; i < this.blockCount; ++i) {
-        GameObject o;
-
-        if (i == abp) {
-          o = Instantiate(this.addBulletProp, Vector3.zero, Quaternion.identity);
-        } else {
-          var r = Random.Range(0, 10);
-          if (r < 5) {
-            o = Instantiate(this.block, Vector3.zero, Quaternion.identity);
-            o.GetComponent<Block>().SetHealth(GameManager.score);
-          } else if (r < 8) {
-            o = Instantiate(this.diamondBlock, Vector3.zero, Quaternion.identity);
-            o.GetComponent<Block>().SetHealth(GameManager.score);
-          } else
-            o = null;
-        }
-        if (o != null) {
-          o.transform.position = new Vector3((i - (this.blockCount - 1) / 2.0f) * this.blockSpacing, this.initBlockY, 0);
-          o.transform.parent = props.transform;
+          GameObject.Find("Shooter").GetComponent<Shooter>().UpdateProps();
         }
       }
     }
--- Assets/Naive/Scripts/Shooter.cs
+++ Assets/Naive/Scripts/Shooter.cs
@@ -10,12 +10,22 @@ namespace BBTAN.Naive {
     [SerializeField] GameObject bullet;
     [SerializeField] float intervalMs = 100;
 
+    [SerializeField] int blockCount = 5;
+    [SerializeField] GameObject block;
+    [SerializeField] GameObject diamondBlock;
+    [SerializeField] GameObject addBulletProp;
+    [SerializeField] float initBlockY = 5;
+    [SerializeField] float blockSpacing = 0.5f;
+
     LineRenderer line;
 
     void Start() {
       this.line = this.GetComponent<LineRenderer>();
       this.line.positionCount = 2;
       this.line.SetPosition(0, this.transform.position);
+
+      // generate level 1
+      this.UpdateProps();
     }
 
     void Update() {
@@ -59,6 +69,39 @@ namespace BBTAN.Naive {
       this.transform.Find("Canvas").Find("BallCountText").GetComponent<TMP_Text>().text = "x" + GameManager.ballCount.ToString();
       this.line.positionCount = 2;
       this.line.SetPosition(0, this.transform.position);
+    }
+
+    public void UpdateProps() {
+      var props = GameObject.Find("Props");
+      // move all existing props
+      for (var i = 0; i < props.transform.childCount; ++i) {
+        props.transform.GetChild(i).transform.Translate(Vector2.down * this.blockSpacing);
+      }
+
+      // index of add bullet prop
+      var abp = Random.Range(0, this.blockCount);
+
+      for (var i = 0; i < this.blockCount; ++i) {
+        GameObject o;
+
+        if (i == abp) {
+          o = Instantiate(this.addBulletProp, Vector3.zero, Quaternion.identity);
+        } else {
+          var r = Random.Range(0, 10);
+          if (r < 5) {
+            o = Instantiate(this.block, Vector3.zero, Quaternion.identity);
+            o.GetComponent<Block>().SetHealth(GameManager.score);
+          } else if (r < 8) {
+            o = Instantiate(this.diamondBlock, Vector3.zero, Quaternion.identity);
+            o.GetComponent<Block>().SetHealth(GameManager.score);
+          } else
+            o = null;
+        }
+        if (o != null) {
+          o.transform.position = new Vector3((i - (this.blockCount - 1) / 2.0f) * this.blockSpacing, this.initBlockY, 0);
+          o.transform.parent = props.transform;
+        }
+      }
     }
   }
 }
