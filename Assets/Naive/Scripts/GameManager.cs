3063d352626b1e2a0bf9b955ba397c43bcff2e4b
DiscreteTom <discrete_tom@outlook.com>
2022-09-13 20:40:16 +0800

    can shoot bullet

---
+++ Assets/Naive/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+  static public bool shooting = false;
+  static public int score = 1;
+  static public int highScore = 0;
+  static public int ballCount = 1;
+}
