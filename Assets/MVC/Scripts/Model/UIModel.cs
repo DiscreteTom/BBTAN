8b2d3dacc1abdee2fd606a1b123d39cc318e13ae
DiscreteTom <discrete_tom@outlook.com>
2022-09-15 19:59:40 +0800

    rename files

---
+++ Assets/MVC/Scripts/Model/UIModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using DT.General;
+using UnityEngine;
+
+namespace BBTAN.MVC {
+  public class UIModel {
+    static public Watch<int> score;
+    static public Watch<int> highScore;
+  }
+}
