3c85dd068a175e0a41fd247002abf2f8ba8be5c3
DiscreteTom <discrete_tom@outlook.com>
2022-09-17 22:07:23 +0800

    optimize code

--- Assets/MVC/Scripts/App.cs
+++ Assets/MVC/Scripts/App.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 namespace BBTAN.MVC {
   public class App : MonoBehaviour {
     LevelModel model;
+
+    // controllers
     UIController ui;
+    ShooterController shooter;
 
     void Start() {
-      // init
+      // init model
       this.model = new LevelModel();
-      this.ui = new UIController(this.transform.Find("ScoreText").GetComponent<TMP_Text>(), this.transform.Find("HighScoreText").GetComponent<TMP_Text>());
 
-      // inject
-      this.ui.Inject(model);
+      // init controllers
+      var scoreText = this.transform.Find("ScoreText").GetComponent<TMP_Text>();
+      var highScoreText = this.transform.Find("HighScoreText").GetComponent<TMP_Text>();
+      this.ui = new UIController(model, scoreText, highScoreText);
+      var shooterView = this.transform.Find("Shooter").GetComponent<Shooter>();
+      this.shooter = new ShooterController(shooterView);
     }
 
     void Update() {
--- Assets/MVC/Scripts/Controller/UIController.cs
+++ Assets/MVC/Scripts/Controller/UIController.cs
@@ -6,17 +6,9 @@ using UnityEngine;
 
 namespace BBTAN.MVC {
   public class UIController {
-    TMP_Text scoreText;
-    TMP_Text highScoreText;
-
-    public UIController(TMP_Text scoreText, TMP_Text highScoreText) {
-      this.scoreText = scoreText;
-      this.highScoreText = highScoreText;
-    }
-
-    public void Inject(LevelModel model) {
-      model.Score.AddListener((v, _) => this.scoreText.text = v.ToString());
-      model.HighScore.AddListener((v, _) => this.highScoreText.text = "TOP:" + v.ToString());
+    public UIController(LevelModel model, TMP_Text scoreText, TMP_Text highScoreText) {
+      model.Score.AddListener((v, _) => scoreText.text = v.ToString());
+      model.HighScore.AddListener((v, _) => highScoreText.text = "TOP:" + v.ToString());
     }
   }
 }
