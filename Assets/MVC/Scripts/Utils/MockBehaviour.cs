e94d43f67b6071cf97e89a8fa286356381f16026
DiscreteTom <discrete_tom@outlook.com>
2022-09-27 22:30:04 +0800

    rename folder

--- Assets/MVC/Scripts/Utils/MockBehaviour.cs
+++
@@ -1,7 +0,0 @@
-namespace DT.General {
-  public class MockBehaviour {
-    public void Init() { }
-    public void Connect() { }
-    public void Update() { }
-  }
-}
--- Assets/MVC/Scripts/Utils/Watch.cs
+++ Assets/MVC/Scripts/Utils/Watch.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine.Events;
 
 namespace DT.General {
